1db37311fbaf95adf717022912e0a5755cfefade
Bachiuss <94529841+Bachiuss@users.noreply.github.com>
2024-03-29 13:04:48 +0400

    setting up character functionally

---
+++ Source/SkateBoardSimulation/SkateboardGuy.cpp
@@ -0,0 +1,91 @@
+#include "SkateboardGuy.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+ASkateboardGuy::ASkateboardGuy() :
+	BaseTurnRate(45.f),
+	BaseLookUpRate(45.f)
+{
+ 	
+	PrimaryActorTick.bCanEverTick = true;
+
+	// SpringArmComponent
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera Boom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.f;
+	CameraBoom->bUsePawnControlRotation = true; // rotate the springarm based on the controller
+
+	// Camera Component
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Follow Camera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // attaching to the end of the springarm
+	FollowCamera->bUsePawnControlRotation = false;
+}
+
+
+void ASkateboardGuy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASkateboardGuy::MoveForward(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.f))
+	{
+		// forward
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0, Rotation.Yaw, 0 };
+
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::X) };
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ASkateboardGuy::MoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.f))
+	{
+		// right
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0, Rotation.Yaw, 0 };
+
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::Y) };
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ASkateboardGuy::TurnAtRate(float Rate)
+{
+	// calculate delta from the rate
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ASkateboardGuy::LookUpAtRate(float Rate)
+{
+	// calculate delta from the rate
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+
+void ASkateboardGuy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+void ASkateboardGuy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASkateboardGuy::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASkateboardGuy::MoveRight);
+	PlayerInputComponent->BindAxis("TurnRate", this, &ASkateboardGuy::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &ASkateboardGuy::LookUpAtRate);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+}
+
---
+++ Source/SkateBoardSimulation/SkateboardGuy.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SkateboardGuy.generated.h"
+
+UCLASS()
+class SKATEBOARDSIMULATION_API ASkateboardGuy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	
+	ASkateboardGuy();
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+	void TurnAtRate(float Rate);
+	void LookUpAtRate(float Rate);
+
+private:	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float BaseTurnRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float BaseLookUpRate;
+
+public:
+	// It returns the springarm's subobject
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	// It returns followcamera's subobject
+	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+};
